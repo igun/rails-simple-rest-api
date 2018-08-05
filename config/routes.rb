9f6fae3e8e2271a0204a02515158b7484adff416
Gunawan Prasetia <gunawan.ep@gmail.com>
2018-08-05 21:40:07 +0700

    add book model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   constraints subdomain: 'api' do
     api_version(:module => "V1", :path => {:value => "v1"}) do
-
+      resources :books
     end  
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
